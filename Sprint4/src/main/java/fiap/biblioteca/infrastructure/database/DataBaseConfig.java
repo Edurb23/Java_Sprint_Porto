e3509b3c6c3a55b29dd292c15993374702e28f78
Edurb23 <rm551987@fiap.com.br>
2023-11-13 19:21:26 -0300

    trocando as credenciais do banck

--- Sprint4/src/main/java/fiap/biblioteca/infrastructure/database/DataBaseConfig.java
+++ Sprint4/src/main/java/fiap/biblioteca/infrastructure/database/DataBaseConfig.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DataBaseConfig {
     private static final String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
-    private static final String USERNAME = "rm550821";
-    private static final String PASSWORD = "fiap23";
+    private static final String USERNAME = "rm98525";
+    private static final String PASSWORD = "230493";
 
 
     public static Connection getConnection() throws SQLException {
